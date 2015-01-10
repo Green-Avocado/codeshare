fac71afbfbff5ca90cdd3a7079d98b135fcb6f19
mario.moreno <mario.moreno@globant.com>
2015-01-10 16:40:59 -0300

    updated info, codeshare 2015!

--- Main/Src/Common/GlobalAssemblyInfo.cs
+++ Main/Src/Common/GlobalAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Code Share")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
