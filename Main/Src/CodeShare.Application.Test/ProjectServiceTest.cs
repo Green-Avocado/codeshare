786e746e243d185ee7f0a2575e4c7e979397c194
Mario Moreno <mario.moreno@live.com>
2015-03-22 17:25:03 -0300

    CS-1 Add ProjectServiceTest class

---
+++ Main/Src/CodeShare.Application.Test/ProjectServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CodeShare.Data;
+using CrossCutting.Core.Logging;
+using CrossCutting.MainModule.IOC;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeShare.Application.Test
+{
+    [TestClass]
+    public class ProjectServiceTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorWithNullUnitOfWorkShouldThrowArgumentNullException()
+        {
+            // Arrange
+            IUnitOfWork unitOfWork = null;
+            var logManager = IocUnityContainer.Instance.Resolve<ILogManager>();
+
+            // Act
+            new ProjectService(unitOfWork, logManager);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorWithNullLogManagerShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var unitOfWork = IocUnityContainer.Instance.Resolve<IUnitOfWork>();
+            ILogManager logManager = null;
+
+            // Act
+            new ProjectService(unitOfWork, logManager);
+        }
+
+        [TestMethod]
+
+    }
+}
